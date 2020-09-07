931bae348d5b8b9b4ebb8278d89656ef323e47af
Chili <54773855+Chililove@users.noreply.github.com>
2020-09-07 17:33:55 +0200

    testcommit

---
+++ SDS/SDS.Core/Application Service/IAvatarService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SDS.Core.Entity;
+
+namespace SDS.Core.Application_Service
+{
+    public interface IAvatarService
+    {
+
+        public List<Avatar> ReadAllAvatars();
+        Avatar Create(Avatar avatar);
+
+        Avatar Update(Avatar avatar);
+
+        Avatar Delete(int id);
+        Avatar ReadById(int id);
+    }
+}
---
+++ SDS/SDS.Core/Application Service/Service/AvatarService.cs
@@ -0,0 +1,70 @@
+﻿using SDS.Core.Domain_Service;
+using SDS.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SDS.Core.Application_Service.Service
+{
+    public class AvatarService : IAvatarService
+    {
+        private readonly IAvatarRepository _aRepo;
+        public static IEnumerable<Avatar> avatarList;
+
+
+        public AvatarService(IAvatarRepository aRepo)
+        {
+
+            _aRepo = aRepo;
+        }
+
+        public Avatar Create(Avatar avatar)
+        {
+            if (avatar.Name.Length < 1)
+            {
+                throw new InvalidDataException("Name must be atleast 1 char");
+            }
+            return _aRepo.Create(avatar);
+
+        }
+        public Avatar Update(Avatar avatar)
+        {
+            if (avatar.Name.Length < 1)
+            {
+                throw new InvalidDataException("Name must be atleast 1 char");
+            }
+            return _aRepo.Update(avatar);
+
+        }
+
+        public List<Avatar> ReadAllAvatars()
+        {
+            return _aRepo.GetAllAvatars();
+        }
+
+        public Avatar Delete(Avatar avatar)
+        {
+            if (avatar.Id < 1)
+            {
+
+                throw new InvalidDataException("Id must be atleast 1");
+            }
+            return _aRepo.Delete(avatar);
+        }
+
+
+        public void ListAvatars()
+        {
+            foreach (var avatar in avatarList)
+            {
+                Console.WriteLine($"Id: {avatar.Id}\n Name: {avatar.Name}\n Type: {avatar.Type}\n Birthdate: {avatar.Birthday}\n Sold date: {avatar.SoldDate}\n Color: {avatar.Color}\n Previous Owner: {avatar.PreviousOwner}\n Price: {avatar.Price}");
+            }
+        }
+
+        public Avatar ReadById(int id)
+        {
+            return _aRepo.GetAvatarById(id);
+        }
+    }
+}
